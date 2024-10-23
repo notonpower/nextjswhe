fd05fafa747d7f3a29e65395bbba613271bbbf3e
Noton450 <79391702+Noton450@users.noreply.github.com>
2024-10-24 00:58:11 +0900

    moudame

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
     images: {
       unoptimized: true,
     },
-    basePath: '', // リポジトリ名を削除
-  assetPrefix: '', // リポジトリ名を削除
-    images: { unoptimized: true } ,
+    basePath: '/nextjswhe',
+    assetPrefix: '/nextjswhe/',
     trailingSlash: true,
     webpack: (config) => {
       config.resolve.fallback = { fs: false, path: false };
